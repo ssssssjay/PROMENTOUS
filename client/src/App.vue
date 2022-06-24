65f0535b6a69210d5d4cd75411c316b27d556008
joansdev <joans.dev@gmail.com>
2022-06-25 04:57:43 +0900

    app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <HeaderLayout />
   <router-view />
+  <FooterLayout />
 </template>
-
-<style lang="scss">
+<script>
+import HeaderLayout from "@/components/layouts/HeaderLayout.vue";
+import FooterLayout from "@/components/layouts/FooterLayout.vue";
+export default {
+  components: {
+    HeaderLayout,
+    FooterLayout
+  }
+};
+</script>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,17 +21,14 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
