564966ba5d7def5b24d35fba365e40559060d37b
권유리 <99jiyusu@gmail.com>
2022-06-25 19:47:35 +0900

    Feat: 프로젝트 모집글 상세 페이지 추가

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -24,6 +24,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "mentolist" */ "../views/MentoListView.vue")
+  },
+  {
+    path: "/projectdetail",
+    name: "projectdetail",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "projectdetail" */ "../views/ProjectDetailView.vue"
+      )
   }
 ];
 
