40bf8c66dd168b63ae391b0f8e27801758967898
evelo0702 <89081441+evelo0702@users.noreply.github.com>
2022-06-25 23:14:05 +0900

    sdfsdfxcv
    sdfsdfwer

--- client/src/main.js
+++ client/src/main.js
@@ -4,4 +4,6 @@ import router from "./router";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@/styles/abc.css";
 createApp(App).use(store).use(router).mount("#app");
