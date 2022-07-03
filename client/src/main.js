fbbb36268efa652c79ea57d00a748283e20d3bb6
inhoKim <unoestellar@naver.com>
2022-07-03 16:46:27 +0900

    0703  1646

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import mixins from "./mixins";
+import "@/styles/reset.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -11,10 +12,10 @@ import "@/styles/ih_kim.css";
 import "@/styles/yuri.css";
 import Datepicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
-import "@/styles/reset.css";
 
 createApp(App)
   .use(store)
   .use(router)
+  .mixin(mixins)
   .component("Datepicker", Datepicker)
   .mount("#app");
