b83daf8937e061b51734fd285102f627be40432c
joansdev <joans.dev@gmail.com>
2022-07-10 18:57:57 +0900

    Feat: 라우터에 Components 등록

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -66,7 +66,7 @@ const routes = [
   {
     path: "/mypageinfo",
     name: "MyPageView",
-    // route level code-splitting
+    // route level code-splittingd
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
