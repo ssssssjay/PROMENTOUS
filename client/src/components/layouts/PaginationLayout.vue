0ee6411f8b60a9f7b137064d678bad7403cbe7b3
Sungjae.Kwak <sungjae8970@gmail.com>
2022-07-17 17:27:56 +0900

    Feat 부모로 페이지 데이터 props로 받고, emit추가

--- client/src/components/layouts/PaginationLayout.vue
+++ client/src/components/layouts/PaginationLayout.vue
@@ -2,22 +2,25 @@
   <div>
     <nav aria-label="Page navigation example">
       <ul class="pagination" id="nav">
+        <!-- 전페이지 -->
+        <!-- <li class="page-item disabled"> -->
         <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
+          <a class="page-link" aria-label="Previous">
             <span aria-hidden="true" style="color: #0bc0eb">&laquo;</span>
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link" href="#" style="color: #0bc0eb">1</a>
+        <!-- 숫자들 -->
+        <li class="page-item" v-for="(data, index) in page" :key="index">
+          <button
+            class="page-link"
+            @click="$emit('paging', data)"
+            style="color: #0bc0eb">
+            {{ data }}
+          </button>
         </li>
+        <!-- 다음페이지 -->
         <li class="page-item">
-          <a class="page-link" href="#" style="color: #0bc0eb">2</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#" style="color: #0bc0eb">3</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
+          <a class="page-link" aria-label="Next">
             <span aria-hidden="true" style="color: #0bc0eb">&raquo;</span>
           </a>
         </li>
@@ -26,12 +29,14 @@
   </div>
 </template>
 <script>
+// this.page로 접근
 export default {
+  props: {
+    page: Number
+  },
   components: {},
   data() {
-    return {
-      sampleData: ""
-    };
+    return {};
   },
   setup() {},
   created() {},
