aded07d64b3431f31517b2020f1cd38313bdec4f
Sungjae.Kwak <sungjae8970@gmail.com>
2022-07-25 19:01:24 +0900

    Design: 렌더링 문구 및 스타일 변경

--- client/src/components/CardList.vue
+++ client/src/components/CardList.vue
@@ -3,9 +3,12 @@
     <section class="project_list">
       <div class="card" v-for="(project, i) in projects" :key="i">
         <!-- TODO: 밑의 goToDetail안의 인자는 API 연결이후 project.project_id로 변경해야할 듯 -->
-        <section class="card-body" @click="goToDetail(project.project_id)">
+        <section
+          class="card-body"
+          :class="project.status_code === '모집중' ? '' : 'fin'"
+          @click="goToDetail(project.project_id)">
           <div class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-start text-muted"
+            <span class="text-start text-muted desc-exp-date"
               >시작예정일 : {{ project.exp_start_date }}</span
             >
             <span class="project-status">{{ project.status_code }}</span>
@@ -51,7 +54,7 @@
               {{ project.totalPeople }}</span
             >
             <button class="progressMethod btn-primary">
-              {{ project.progress_method }}
+              {{ project.progress_method === "ON" ? "온라인" : "오프라인" }}
             </button>
           </div>
         </section>
@@ -100,6 +103,18 @@ export default {
   margin-right: 48px;
   cursor: pointer;
   transition: all 0.3s;
+  position: relative;
+}
+.project_list .fin::after {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  border-radius: 15px;
+  content: "";
+  width: 100%;
+  height: 250px;
+  background-color: rgba($color: #fff, $alpha: 0.5);
 }
 .project_list .card:nth-child(4n) {
   margin-right: 0;
@@ -109,6 +124,9 @@ export default {
   box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset,
     rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
     rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+}
+.desc-exp-date {
+  font-size: 15px;
 }
 .stack-icon {
   font-size: 12px;
@@ -123,10 +141,10 @@ export default {
 .project-status {
   display: inline-block;
   padding: 0 12px;
-  font-size: 12px;
+  font-size: 13px;
   text-align: center;
   border-radius: 28px;
-  border: 1px solid black;
+  border: 1px solid #000;
   box-sizing: border-box;
 }
 .ellipsis {
@@ -141,9 +159,10 @@ export default {
   height: 40px;
 }
 .image > img {
-  width: 50px;
-  height: 50px;
+  width: 30px;
+  height: 30px;
   border-radius: 50%;
+  margin-bottom: 4px;
 }
 .progressMethod {
   font-size: 15px;
